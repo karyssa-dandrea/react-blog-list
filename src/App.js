a86073a20b72159efb46559fbf6e6a5b9a9a672e
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-06 14:20:37 -0800

    added my map filter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
 import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
@@ -19,6 +20,11 @@ function App() {
   return (
     <section className="main">
       <Header />
+      <main>
+        {blogs.map((blog) => (
+          <BlogCard key={blog.id} {...blog} />
+        ))}
+      </main>
       <Footer />
     </section>
   );
